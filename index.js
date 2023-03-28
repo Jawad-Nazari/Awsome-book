03d5a71e27cf2e4a6e402acb618ea90491fd551a
Jawad <Jawad.Nazari@outlook.com>
2023-03-29 01:23:59 +0500

    Added classes and methods

--- index.js
+++ index.js
@@ -1,70 +1,60 @@
-// Get the saved data from localStorage or create an empty array
-const storeData = JSON.parse(localStorage.getItem('Added Books')) || [];
-// Add the books to the HTML
-function addBooks(books) {
-  // Use the map function to iterate over each book and create HTML elements
-  return books.map((book, index) => `
-       <p>${book.title}</p>
-       <p>${book.author}</p>
-       <button class="remove" data-index="${index}" type="button">Remove</button>
-       <hr>
-     `).join('');
-}
-
-// Display the books on the webpage
-function displayBooks() {
-  const container = document.querySelector('.container');
-  const div = document.createElement('div');
-  div.classList.add('book-list');
-  div.innerHTML = addBooks(storeData);
-  container.innerHTML = '';
-  container.appendChild(div);
-}
-// Save the data to localStorage
-function updateData() {
-  localStorage.setItem('Added Books', JSON.stringify(storeData));
-}
-
-// Remove a book from the storeData array
-function removeBook(index) {
-  storeData.splice(index, 1);
-  updateData();
-  displayBooks();
-}
-
-// Defining addNewBook function
-function addNewBook(title, author) {
-  // Create a new book object
-  const newBook = {
-    title,
-    author,
-  };
-  // Add the new book to the storeData array
-  storeData.push(newBook);
-
-  // Update the data in localStorage and display the books
-  updateData();
-  displayBooks();
-}
-
-// Add an event listener to the remove button
-const container = document.querySelector('.container');
-container.addEventListener('click', (event) => {
-  if (event.target.classList.contains('remove')) {
-    const { index } = event.target.dataset;
-    removeBook(index);
+class Book {
+  constructor(title, author) {
+    this.title = title;
+    this.author = author;
   }
-});
+}
 
-// Add an event listener to the form submit button
-const form = document.getElementById('form');
-form.addEventListener('submit', (event) => {
-  event.preventDefault();
-  const title = document.querySelector('.title').value;
-  const author = document.querySelector('.author').value;
-  addNewBook(title, author);
-  form.reset();
-});
+class BookList {
+  constructor() {
+    this.storeData = JSON.parse(localStorage.getItem('Added Books')) || [];
+    this.container = document.querySelector('.container');
+    this.form = document.getElementById('form');
+    this.form.addEventListener('submit', this.addNewBook.bind(this));
+    this.container.addEventListener('click', this.removeBook.bind(this));
+    this.displayBooks();
+  }
 
-// Display the books when the page loads
-displayBooks();
+  addNewBook(event) {
+    event.preventDefault();
+    const title = document.querySelector('.title').value;
+    const author = document.querySelector('.author').value;
+    const newBook = new Book(title, author);
+    this.storeData.push(newBook);
+    this.updateData();
+    this.displayBooks();
+    this.form.reset();
+  }
+
+  removeBook(event) {
+    if (event.target.classList.contains('remove')) {
+      const index = event.target.dataset.index;
+      this.storeData.splice(index, 1);
+      this.updateData();
+      this.displayBooks();
+    }
+  }
+
+  addBooks() {
+    return this.storeData.map((book, index) => `
+      <p>${book.title}</p>
+      <p>${book.author}</p>
+      <button class="remove" data-index="${index}" type="button">Remove</button>
+      <hr>
+    `).join('');
+  }
+
+  displayBooks() {
+    const div = document.createElement('div');
+    div.classList.add('book-list');
+    div.innerHTML = this.addBooks();
+    this.container.innerHTML = '';
+    this.container.appendChild(div);
+  }
+
+  updateData() {
+    localStorage.setItem('Added Books', JSON.stringify(this.storeData));
+  }
+}
+
+const bookList = new BookList();
